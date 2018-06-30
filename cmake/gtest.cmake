5df83f0dfb79be8b4aba9c1fc2b1bf7833cf34e4
Morgan Foley <hipolipolopigus@gmail.com>
2018-06-30 13:00:24 +1200

    Remove hanging Windows tests, more attempts at pthread fixes.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,6 +1,6 @@
 find_package(GTest REQUIRED)
 
 function(enable_gtest TARGET)
-	target_link_libraries(${TARGET} ${GTEST_BOTH_LIBRARIES})
+	target_link_libraries(${TARGET} ${GTEST_BOTH_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 	target_include_directories(${TARGET} PUBLIC ${GTEST_INCLUDE_DIRS})
 endfunction()
--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -2,5 +2,6 @@
 if (WIN32)
 	add_definitions(-DFOO)
 elseif (UNIX)
+	find_package(Threads)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -lpthread -lm")
 endif ()
