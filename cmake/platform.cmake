23676add01d0de85e27307797e689e4e308ac151
Morgan Foley <hipolipolopigus@gmail.com>
2018-06-30 12:51:35 +1200

    [Grumbling about *nix]

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -2,6 +2,5 @@
 if (WIN32)
 	add_definitions(-DFOO)
 elseif (UNIX)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
-	find_package (Threads)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -lpthread -lm")
 endif ()
